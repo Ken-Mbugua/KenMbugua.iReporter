02f4d32c7762ee6c0bbedf9dd12ab037807a9b7d
ken-mbugua <wagereka15@hotmail.com>
2018-12-02 18:18:19 +0300

    [Finish #162367789] add incident endpoint working

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -8,4 +8,4 @@ version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(version_one)
 
 # incidents end point
-api.add_resource(Incidents, '/incidents', endpoint='incident_list')
+api.add_resource(Incidents, '/incidents', endpoint='incidents_list')
