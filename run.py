6b37edeeb3c9b1044aac27be12d29cb486b68652
ken-mbugua <wagereka15@hotmail.com>
2018-12-02 17:54:51 +0300

    [FInish #162367783] config-setup commplete

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app
 # import config var from env
 CONFIG_NAME = os.getenv('PROJECT_SETTINGS')
 
-""" 
+"""  
 initialising flask app by calling create_app
 and passing development config
 """
