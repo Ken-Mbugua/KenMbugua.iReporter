3ab51a9345765c873750ef12a54fb409c29b7550
ken-mbugua <wagereka15@hotmail.com>
2018-12-02 18:38:58 +0300

    [Starts #162367796] created get incident url

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,10 +2,12 @@ from flask import Blueprint
 from flask_restful import Api
 
 # imported Incidents from views
-from .views.incidents import Incidents
+from .views.incidents import Incidents, IncidentsId
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(version_one)
 
-# incidents end point
+# incidents end points
 api.add_resource(Incidents, '/incidents', endpoint='incidents_list')
+api.add_resource(IncidentsId, '/incidents/<int:incident_id>',
+                 endpoint='incident')
