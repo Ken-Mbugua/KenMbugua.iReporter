9c6fe8aacf70466357ee41b3be3d1c014b51840f
ken-mbugua <wagereka15@hotmail.com>
2018-12-04 07:38:32 +0300

    [FInish #162368934] added simple fields validation script for update commennt and location enpoints

--- app/api/v1/views/update_incident.py
+++ app/api/v1/views/update_incident.py
@@ -11,22 +11,31 @@ class IncidentsUpdateLocation(Resource, IncidentsModel):
         """
         data = request.get_json()
 
-        new_incident_instance = IncidentsModel()
-        res = new_incident_instance.edit_incident(incident_id, data)
+        # script to allow only location request.body
+        key_list = [*data]
+        if (len(key_list) == 1 and key_list[0] == "location"):
 
-        if res:
-            return {
-                "status": 200,
-                "data": [{
-                    "id": res["id"],
-                    "message": "incident record has been updated"
-                }]
-            }, 200
+            new_incident_instance = IncidentsModel()
+            res = new_incident_instance.edit_incident(incident_id, data)
+
+            if res:
+                return {
+                    "status": 200,
+                    "data": [{
+                        "id": res["id"],
+                        "message": "incident record has been updated"
+                    }]
+                }, 200
+            else:
+                return {
+                    "status": 404,
+                    "error": "Not found for id {}".format(incident_id)
+                }, 404
         else:
             return {
-                "status": 404,
-                "error": "Not found for id {}".format(incident_id)
-            }, 404
+                "status": 403,
+                "error": "Bad Request only location is allowed"
+            }, 403
 
 
 class IncidentsUpdateComment(Resource, IncidentsModel):
@@ -37,19 +46,28 @@ class IncidentsUpdateComment(Resource, IncidentsModel):
         """
         data = request.get_json()
 
-        new_incident_instance = IncidentsModel()
-        res = new_incident_instance.edit_incident(incident_id, data)
+        # script to allow only comment request.body
+        key_list = [*data]
+        if (len(key_list) == 1 and key_list[0] == "comment"):
 
-        if res:
-            return {
-                "status": 200,
-                "data": [{
-                    "id": res["id"],
-                    "message": "incident record has been updated"
-                }]
-            }, 200
+            new_incident_instance = IncidentsModel()
+            res = new_incident_instance.edit_incident(incident_id, data)
+
+            if res:
+                return {
+                    "status": 200,
+                    "data": [{
+                        "id": res["id"],
+                        "message": "incident record has been updated"
+                    }]
+                }, 200
+            else:
+                return {
+                    "status": 404,
+                    "error": "Not found for id {}".format(incident_id)
+                }, 404
         else:
             return {
-                "status": 404,
-                "error": "Not found for id {}".format(incident_id)
-            }, 404
+                "status": 403,
+                "error": "Bad Request only comment is allowed"
+            }, 403
