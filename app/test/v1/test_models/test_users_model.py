826bb1d8741b58b012a14e728ffd75579f10a1a7
ken-mbugua <wagereka15@hotmail.com>
2018-12-04 23:26:04 +0300

    [Feature #162415312] creating user_models tests cases

--- app/test/v1/test_models/test_users_model.py
+++ app/test/v1/test_models/test_users_model.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import datetime
 from app.api.v1.models.users_model import UsersModel
 
 
@@ -6,12 +7,19 @@ class TestUsersModel(TestCase):
     def setUp(self):
         self.user = UsersModel()
         self.user_data = self.user._users_db
-        user_data = {
-
+        self.user_data = {
+            "firstname": "Andeka",
+            "lastname": "Kibiro",
+            "othernames": "AnKibi",
+            "email": "ankibi@andela.com",
+            "phoneNumber": "07168858858",
+            "username": "@Ankibi",
+            "registered": str(datetime.datetime.now()),
+            "isAdmin": "False"
         }
 
     def create_test_user(self):
-        return self.user.create_user(user_data)
+        return self.user.create_user(self.user_data)
 
     def test_create_user(self):
 
