53b6c13d78adb46ec9bc27b50e3b1c58c2bfa28d
ken-mbugua <wagereka15@hotmail.com>
2018-12-10 09:47:09 +0300

    [Feature #162419785] Created user object method to format user data

--- app/api/v2/models/users_model.py
+++ app/api/v2/models/users_model.py
@@ -12,35 +12,44 @@ class UsersModel():
         self._users_db = DbModel()
         self.model_val = ModelValidation()
 
-    def user_details(self, user_details):
+    def user_object(self, user_details):
+        """method to formart user data object"""
+
         return {
-            "email": user_details["email"],
-            "phone_number": user_details["phone_number"],
-            "password_hash": generate_password_hash(user_details["password"])
-            .decode("utf-8"),
-            "username": user_details["username"],
-            "isAdmin": False,
-            "createdBy": 34,
-            "Registered": str(self.created_at),
-            "demo_token": "iwtgrsh7772099gavgfhlwe??@@##gafugiuwef&$$"
+            "auth_token": "iwtgrsh7772099gavgfhlwe??@@##gafugiuwef&$$",
+            "user": {
+                "firstname": "",
+                "username": user_details["username"],
+                "email": user_details["email"],
+                "phone_number": user_details["phone_number"],
+                "password_hash": generate_password_hash
+                (user_details["password"])
+                .decode("utf-8"),
+                "isAdmin": False,
+                "createdBy": 34,
+                "Registered": str(self.created_at)
+            }
         }
 
     def create_user(self, user_details):
 
-        print("User_details::", user_details)
+        user_details = self.user_object(user_details)
 
-        email = self.get_user_by_email(user_details["email"])
+        print("User_details::", user_details)
+        print("Email::", user_details["user"]["email"])
+
+        email = self.get_user_by_email(user_details["user"]["email"])
 
         if email:  # duplicate user found return
             return None
         else:
             query = "INSERT INTO users(email, username, phone_number, " +\
                 " password_hash, auth_token) VALUES (" +\
-                " '"+(user_details["email"]+"', " +
-                      " '"+user_details["username"]+"', " +
-                      " '"+user_details["phone_number"]+"', " +
-                      " '"+user_details["password_hash"]+"', " +
-                      " '"+user_details["demo_token"])+"') "
+                " '"+(user_details["user"]["email"]+"', " +
+                      " '"+user_details["user"]["username"]+"', " +
+                      " '"+user_details["user"]["phone_number"]+"', " +
+                      " '"+user_details["user"]["password_hash"]+"', " +
+                      " '"+user_details["auth_token"])+"') "
 
             # run query then commit record
             self._users_db.query(query)
