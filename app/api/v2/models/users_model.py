de2adedc787a212ef2f836b376401f75e5e4f510
ken-mbugua <wagereka15@hotmail.com>
2018-12-12 14:13:27 +0300

    [Feature #162419785] refactored create user query

--- app/api/v2/models/users_model.py
+++ app/api/v2/models/users_model.py
@@ -7,83 +7,43 @@ from app.db.db_config import DbModel
 
 class UsersModel(DbModel):
 
-    def __init__(self, username=None, firstname=None, email=None, phone_number=None,
-                 password_hash=None, isAdmin=False,
-                 date_created=None):
+    def __init__(self, username=None, firstname=None, email=None,
+                 phone_number=None, password=None,
+                 isAdmin=False, date_created=None):
 
         self.firstname = firstname
         self.username = username
         self.email = email
-        self.password_hash = generate_password_hash
-        (user_details["password"])
-        .decode("utf-8"),
+        self.password_hash = generate_password_hash(
+            password).decode("utf-8")
         self.isAdmin = isAdmin
+        self.phone_number = phone_number
         self.date_created = datetime.datetime.now()
 
         # instanciate the inherited DbModel class
         super().__init__()
 
-    # def user_object(self, user_details):
-    #     """method to formart user data object"""
+    def create_user(self):
 
-    #     return {
-    #         "auth_token": "iwtgrsh7772099gavgfhlwe??@@##gafugiuwef&$$",
-    #         "user": {
-    #             "firstname": "",
-    #             "username": user_details["username"],
-    #             "email": user_details["email"],
-    #             "phone_number": user_details["phone_number"],
-    #             "password_hash": generate_password_hash
-    #             (user_details["password"])
-    #             .decode("utf-8"),
-    #             "isAdmin": False,
-    #             "createdBy": 34,
-    #             "Registered": str(self.date_created)
-    #         }
-    #     }
-
-    def user_res_object(self, user_details):
-        """method to formart user data object"""
-
-        return {
-            "auth_token": "iwtgrsh7772099gavgfhlwe??@@##gafugiuwef&$$",
-            "user": {
-                "firstname": "",
-                "username": user_details["username"],
-                "email": user_details["email"],
-                "phone_number": user_details["phone_number"],
-                "Registered": str(self.date_created)
-            }
-        }
-
-    def create_user(self, user_details):
-
-        user_details = self.user_object(user_details)
-
-        # print("User_details::", user_details)
-        # print("Email::", user_details["user"]["email"])
-
-        email = self.get_user_by_email(user_details["user"]["email"])
+        email = self.get_user_by_email(self.email)
 
         if email:  # duplicate user found return None
             return None
         else:
             query = "INSERT INTO users(email, username, phone_number, " +\
-                " password_hash, auth_token) VALUES (" +\
-                " '"+(user_details["user"]["email"]+"', " +
-                      " '"+user_details["user"]["username"]+"', " +
-                      " '"+user_details["user"]["phone_number"]+"', " +
-                      " '"+user_details["user"]["password_hash"]+"', " +
-                      " '"+user_details["auth_token"])+"') "
+                " password_hash, auth_token) VALUES (%s,%s,%s,%s)"
+
+            data = (self.email, self.username,
+                    self.phone_number, self.password_hash)
 
             # run query then commit record
-            self.query(query)
+            self.query((query, data))
             self.save()
-            return user_details
+            return "create user success"
 
     def get_user_by_email(self, user_email):
 
-        query = "SELECT * from users WHERE email='"+user_email+"'"
+        query = "SELECT * from users WHERE email={}".format(user_email)
         # query db
         self.query(query)
         # return queried records (single record)
