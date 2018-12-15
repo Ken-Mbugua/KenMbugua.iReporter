2b61222439b5e42e04c18975aff12eb09242fa69
ken-mbugua <wagereka15@hotmail.com>
2018-12-15 23:57:52 +0300

    [Feature #162450349] created get_all_incidents endpoint tests

--- app/test/v2/views/test_incidents_data.py
+++ app/test/v2/views/test_incidents_data.py
@@ -2,7 +2,6 @@
 incident1 = {
     "title": "Traffice Corruption",
     "description": "Offices taking bribes",
-    "type": "RedFlag",
     "incident_status": "Rejected",
     "location": "-34444400, 3444499900",
     "image": [
@@ -14,7 +13,7 @@ incident1 = {
         "var/www/uploads/readflags/video/USR-232455.mp4",
         "var/www/uploads/readflags/video/USR-232455.mp4",
     ],
-    "comment": "This is fake all news i tell ya"
+    "comment": "This is all fake news i tell ya"
 
 
 }
@@ -22,8 +21,7 @@ incident1 = {
 
 incident2 = {
     "title": "Manhole Hazard",
-    "description": "Manhole open around hirlingum mall",
-    "incident_type": "Intervention",
+    "description": "Manhole open around hurlingam mall",
     "incident_status": "Rejected",
     "location": "-34444400, 3444499900",
     "image": [
@@ -36,6 +34,61 @@ incident2 = {
         "var/www/uploads/readflags/video/USR-232455.mp4",
     ],
     "comment": "ignore it or cover it up, out of sight out of mind"
+}
+
+incident3 = {
+    "title": "Nepotism",
+    "description": "Retiring mebers of staff were requires to submit their " +
+    " next of kin C.Vs for an assured employment, no interviews or anything",
+    "incident_status": "Under Investigation",
+    "location": "-34444400, 4344499900",
+    "image": [
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+
+    ],
+    "video": [
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+    ],
+    "comment": "This is some next level nepotism"
+}
+
+incident4 = {
+    "title": "Building Collpase Hazard",
+    "description": "A Residential Six floor flat pipeline area is "
+    " stil under construction and occupied upto the fourth floor",
+    "location": "-34444400, 4344499900",
+    "image": [
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+
+    ],
+    "video": [
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+    ],
+    "comment": "How many buildings should collapse for us to learn?"
+}
+
+
+incident5 = {
+    "title": "Hate Speech",
+    "description": "The area MCA (grinko ward) was filmed inciting a " +
+    " group of young men and women that he would never have a person" +
+    " from tribe B into the county assembly.",
+    "incident_status": "Under Investigation",
+    "location": "-34444400, 4344499900",
+    "image": [
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+        "var/www/uploads/incidents/img/USR-232455.jpeg",
+
+    ],
+    "video": [
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+        "var/www/uploads/readflags/video/USR-232455.mp4",
+    ],
+    "comment": "This is some next level tribalism"
 }
 
 incident_no_media_intervention = {
--- app/test/v2/views/test_incidents_v2.py
+++ app/test/v2/views/test_incidents_v2.py
@@ -3,8 +3,9 @@ import os
 import json
 from unittest import TestCase
 from app.db.db_config import DbModel
-from test_incidents_data import incident1, incident2, comment_data,\
-    location_data, incident_no_media_intervention, incident_no_media_redflag
+from test_incidents_data import incident1, incident2, incident3, incident4,\
+    incident5, comment_data, location_data, incident_no_media_intervention,\
+    incident_no_media_redflag
 from test_data_auth import sign_in_data, sign_up_data
 
 
@@ -47,15 +48,26 @@ class TestIncidentsV2(TestCase):
 
         return response
 
-    def add_incident(self, incident_type, incident_data):
+    def add_incident(self, incident_type=None,
+                     incident_data=None, registered=None):
         """
         method to add new incident
+        registered: (boloean) decides to sign_in or sign_up
+        in order to get auth token
         """
 
         # login to get token
-        register_response_data = json.loads(self.signup_user(
-            sign_up_data
-        ).data.decode())
+
+        if registered:
+            # user already registered sign in
+            register_response_data = json.loads(self.signin_user(
+                sign_in_data
+            ).data.decode())
+        else:
+            # user not found register new user
+            register_response_data = json.loads(self.signup_user(
+                sign_up_data
+            ).data.decode())
 
         token = register_response_data["data"][0]["token"]
 
@@ -68,30 +80,100 @@ class TestIncidentsV2(TestCase):
         )
         return new_incident
 
-    def test_create_redflag(self):
+    def get_all_incidents(self, incident_type):
+        """
+        method to add new incident
+        """
+
+        # login to get token
+        register_response_data = json.loads(self.signin_user(
+            sign_in_data
+        ).data.decode())
+
+        token = register_response_data["data"][0]["token"]
+
+        new_incident = self.app.get(
+            "/api/v2/{}".format(incident_type),
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+token
+            }
+        )
+        return new_incident
+
+    def test_create_intervention(self):
         """
         method to test create incident endpoint
         incident_type: intervention
         """
         response = self.add_incident(
-            "interventions", incident_no_media_intervention)
+            "interventions", incident2)
         result = json.loads(response.data)
         print("RESULT::", result)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(result["data"][0]["message"],
                          "created interventions record")
 
-    def test_create_incident(self):
+    def test_create_redflag(self):
         """
         method to test create incident endpoint
         incident_type: intervention
         """
-        response = self.add_incident("redflags", incident_no_media_redflag)
+        response = self.add_incident("redflags", incident1)
         result = json.loads(response.data)
         print("RESULT::", result)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(result["data"][0]["message"],
                          "created redflags record")
+
+    def test_get_all_interventions(self):
+        """
+        method to test create incident endpoint
+        incident_type: interventions
+        """
+        # add mock data to databse 2 records
+        self.add_incident("interventions", incident2)
+        self.add_incident("interventions", incident4, True)
+
+        # get all incidents
+        response = self.get_all_incidents(
+            "interventions")
+        result = json.loads(response.data)
+        print("RESULT::", result)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["data"][0]["comment"],
+                         "This is all fake news i tell ya")
+        self.assertEqual(result["data"][0]["title"], "Traffice Corruption")
+        self.assertEqual(result["data"][1]["title"], "Manhole Hazard")
+        self.assertEqual(result["data"][0]["incident_type"], "interventions")
+        # should return 2 records
+        self.assertEqual(len(result["data"]), 2)
+        # assert data: title, incident_type, comment
+
+    def test_get_all_redflags(self):
+        """
+        method to test create incident endpoint
+        incident_type: redflags
+        """
+        # add mock data to databse 3 records
+        self.add_incident("redflags", incident1)
+        self.add_incident("redflags", incident3, True)
+        self.add_incident("redflags", incident5, True)
+
+        # get all red flags 2 records should be returned
+        response = self.get_all_incidents(
+            "redflags")
+        result = json.loads(response.data)
+        print("RESULT::", result)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result["data"][0]["comment"],
+                         "This is all fake news i tell ya")
+        self.assertEqual(result["data"][2]["comment"],
+                         "This is some next level nepotism")
+        self.assertEqual(result["data"][0]["title"], "Traffice Corruption")
+        self.assertEqual(result["data"][0]["incident_type"], "redflags")
+        # should return two records
+        self.assertEqual(len(result["data"]), 3)
 
     def tearDown(self):
         """empty table data after each test"""
