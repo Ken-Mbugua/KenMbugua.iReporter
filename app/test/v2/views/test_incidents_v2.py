829ead2185ca81154e5b1008b4bfa731889700fc
ken-mbugua <wagereka15@hotmail.com>
2018-12-16 15:34:50 +0300

    [Starts #162450930] created delete incidents endpoin tests + corrected Truncate table statement to rest identity

--- app/test/v2/views/test_incidents_v2.py
+++ app/test/v2/views/test_incidents_v2.py
@@ -60,16 +60,11 @@ class TestIncidentsV2(TestCase):
 
         if registered:
             # user already registered sign in
-            register_response_data = json.loads(self.signin_user(
-                sign_in_data
-            ).data.decode())
+            token = self.get_token("signin")
+
         else:
             # user not found register new user
-            register_response_data = json.loads(self.signup_user(
-                sign_up_data
-            ).data.decode())
-
-        token = register_response_data["data"][0]["token"]
+            token = self.get_token("register")
 
         new_incident = self.app.post(
             "/api/v2/{}".format(incident_type), data=json.dumps(incident_data),
@@ -86,14 +81,42 @@ class TestIncidentsV2(TestCase):
         """
 
         # login to get token
-        register_response_data = json.loads(self.signin_user(
-            sign_in_data
-        ).data.decode())
-
-        token = register_response_data["data"][0]["token"]
+        token = self.get_token("signin")
 
         new_incident = self.app.get(
             "/api/v2/{}".format(incident_type),
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+token
+            }
+        )
+        return new_incident
+
+    def get_token(self, auth_type="register"):
+
+        if auth_type == "register":
+            # user not found register new user
+            register_response_data = json.loads(self.signup_user(
+                sign_up_data
+            ).data.decode())
+        else:
+            register_response_data = json.loads(self.signin_user(
+                sign_in_data
+            ).data.decode())
+
+        token = register_response_data["data"][0]["token"]
+
+        return token
+
+    def delete_incident(self, incident_type, incident_id):
+        """
+        method to delete incident, based on incident_id
+        """
+        # login to get token
+        token = self.get_token("signin")
+
+        new_incident = self.app.delete(
+            "/api/v2/{}/{}".format(incident_type, incident_id),
             headers={
                 "Content-Type": "application/json",
                 "Authorization": "Bearer "+token
@@ -110,8 +133,10 @@ class TestIncidentsV2(TestCase):
             "interventions", incident2)
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(result["data"][0]["message"],
-                         "created interventions record")
+        self.assertEqual(
+            result["data"][0]["message"],
+            "created interventions record"
+        )
 
     def test_create_intervention_bad_url(self):
         """ bad url test"""
@@ -121,8 +146,10 @@ class TestIncidentsV2(TestCase):
         result = json.loads(response.data)
         print("RESULT::", result)
         self.assertEqual(response.status_code, 405)
-        self.assertEqual(result["error"],
-                         "Invalid Endpoint interventionz ")
+        self.assertEqual(
+            result["error"],
+            "Invalid Endpoint interventionz "
+        )
 
     def test_create_intervention_validation(self):
         """
@@ -130,13 +157,14 @@ class TestIncidentsV2(TestCase):
         validation
         incident_type: intervention
         """
-
         response = self.add_incident(
             "interventions", incident2_invalid_field)
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(result["error"],
-                         "Invalid fields ['my_field', 'responder']")
+        self.assertEqual(
+            result["error"],
+            "Invalid fields ['my_field', 'responder']"
+        )
 
     def test_create_redflag(self):
         """
@@ -147,8 +175,10 @@ class TestIncidentsV2(TestCase):
         result = json.loads(response.data)
         print("RESULT::", result)
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(result["data"][0]["message"],
-                         "created redflags record")
+        self.assertEqual(
+            result["data"][0]["message"],
+            "created redflags record"
+        )
 
     def test_get_all_interventions(self):
         """
@@ -167,13 +197,19 @@ class TestIncidentsV2(TestCase):
 
         # assert data: title, incident_type, comment
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(result["data"][0]["comment"],
-                         "ignore it or cover it up, out of sight out of mind")
-        self.assertEqual(result["data"][1]["comment"],
-                         "How many buildings should collapse for us to learn?")
+        self.assertEqual(
+            result["data"][0]["comment"],
+            "ignore it or cover it up, out of sight out of mind"
+        )
+        self.assertEqual(
+            result["data"][1]["comment"],
+            "How many buildings should collapse for us to learn?"
+        )
         self.assertEqual(result["data"][0]["title"], "Manhole Hazard")
-        self.assertEqual(result["data"][1]["title"],
-                         "Building Collpase Hazard")
+        self.assertEqual(
+            result["data"][1]["title"],
+            "Building Collpase Hazard"
+        )
         self.assertEqual(result["data"][0]["incident_type"], "interventions")
         # should return 2 records
         self.assertEqual(len(result["data"]), 2)
@@ -189,19 +225,93 @@ class TestIncidentsV2(TestCase):
         self.add_incident("redflags", incident5, True)
 
         # get all red flags 2 records should be returned
-        response = self.get_all_incidents(
-            "redflags")
+        response = self.get_all_incidents("redflags")
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(result["data"][0]["comment"],
-                         "This is all fake news i tell ya")
-        self.assertEqual(result["data"][2]["comment"],
-                         "This is some next level tribalism")
+        self.assertEqual(
+            result["data"][0]["comment"],
+            "This is all fake news i tell ya"
+        )
+        self.assertEqual(
+            result["data"][2]["comment"],
+            "This is some next level tribalism"
+        )
         self.assertEqual(result["data"][0]["title"], "Traffice Corruption")
         self.assertEqual(result["data"][0]["incident_type"], "redflags")
         # should return two records
         self.assertEqual(len(result["data"]), 3)
 
+    def test_delete_redflag(self):
+        """
+        method to test redflag deletion
+        incident_type: redflags
+        """
+        # add mock data to databse: 2 records
+        res_red_flag_a = self.add_incident("redflags", incident1)
+        res_red_flag_b = self.add_incident("redflags", incident3, True)
+
+        # retrieve data (incident_id) from inserted records
+        res_red_flag_a = json.loads(res_red_flag_a.data)
+        res_red_flag_b = json.loads(res_red_flag_b.data)
+
+        incident_id_a = res_red_flag_a["data"][0]["id"]
+        incident_id_b = res_red_flag_b["data"][0]["id"]
+
+        # delete red flags  by id
+        response = self.delete_incident(
+            "redflags", incident_id_a
+        )
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            result["data"][0]["message"],
+            "Successfully Deleted redflags"
+        )
+        self.assertEqual(
+            result["data"][0]["id"],
+            incident_id_a
+        )
+
+        # query redflags only one should be left
+        result_get_all = self.get_all_incidents("redflags")
+        self.assertEqual(len(result_get_all["data"]), 1)
+
+    def test_delete_intervention(self):
+        """
+        method to test intervention deletion
+        incident_type: interventions
+        """
+        # add mock data to databse: 2 records
+        res_intv_a = self.add_incident("interventions", incident1)
+        res_intv_b = self.add_incident("interventions", incident3, True)
+
+        # retrieve data (incident_id) from inserted records
+        res_intv_a = json.loads(res_intv_a.data)
+        res_intv_b = json.loads(res_intv_b.data)
+
+        incident_id_a = res_intv_a["data"][0]["id"]
+        incident_id_b = res_intv_b["data"][0]["id"]
+
+        # delete red flags  by id
+        response = self.delete_incident(
+            "interventions", incident_id_a
+        )
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            result["data"][0]["message"],
+            "Successfully Deleted interventions"
+        )
+        self.assertEqual(
+            result["data"][0]["id"],
+            incident_id_a
+        )
+
+        # query interventions only one should be left
+        result_get_all = self.get_all_incidents("interventions")
+        self.assertEqual(len(result_get_all["data"]), 1)
+
     def tearDown(self):
         """empty table data after each test"""
         self.db.truncate_tables()
+        print("DB_TRUNCATED!!")
